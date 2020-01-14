9e989bab2ef20f66b34bf6e5879cade21303350d
jiawang1 <thisisfuture@gmail.com>
2020-01-14 16:45:14 +0800

    chore: remove reference to central css bundle

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom';
 // Import Customized Components
 import App from './App';
 
-// Import Css
-import '@sf/ffr-components/dist/index.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
