3b8162e724d649424b5c0f1199c377d3f662f550
jiawang1 <thisisfuture@gmail.com>
2020-01-16 13:12:17 +0800

    feat: support typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-// Import React Framework
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -6,7 +5,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 // Import Css
-import '@sf/ffr-components/dist/index.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
