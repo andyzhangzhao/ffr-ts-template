9b4c465bddf01104253c22193663526b81aa1c7c
jiawang1 <thisisfuture@gmail.com>
2020-04-04 15:07:52 +0800

    chore: enhance type map

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -2,4 +2,4 @@ import { globalState } from '@sf/ffr-core/es';
 import { HomePageModel } from './homepage/HomePageModel';
 import { DetailModel } from './detail/DetailModel';
 
-export type TGlobalState = globalState<typeof HomePageModel & typeof DetailModel>;
+export type TGlobalState = globalState<typeof HomePageModel | typeof DetailModel>;
