613844e3a4b814ab08e960efbe57f5846787f060
jiawang1 <thisisfuture@gmail.com>
2020-07-04 15:36:00 +0800

    feat: move public url to env

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,8 @@ const buildLib = require('@sf/ffr-core/es/scripts/build');
 const analyze = require('@sf/ffr-core/es/scripts/analyze');
 
 const configPath = path.join(__dirname, '../config');
-
+// eslint-disable-next-line import/no-dynamic-require
+require(`${configPath}/env`); // initialize PUBLIC_URL
 const param = process.argv.slice(-1)[0];
 
 if (param === 'analyze') {
